247446a40300cd98fe7079aa488e52f4a10e218f
Александр Полушкин <aa.polushkin@yandex.ru>
2023-08-30 21:39:03 +0300

    unit 28.6.4 complite

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -2,30 +2,29 @@
 let session = new Map();
 
 // Сохраняем информацию о браузере клиента
-session.set('userAgent', window.navigator.userAgent);
+session.set("userAgent", window.navigator.userAgent);
 
 // Запросим возраст пользователя и сохраним в переменную
 let age = prompt("Пожалуйста, введите ваш возраст");
  
 if (age >= 18) {
     // Сохраняем возраст пользователя
-    session.set('age', age);
+    session.set("age", age);
 
     // Сохраняем время начала сессии
-    session.set('startDate', new Date().toLocaleString());
+    session.set("startDate", new Date().toLocaleString());
 
     // Те, кто старше 18, увидят приветствие и будут направлены на сайт
-    alert("Приветствуем на LifeSpot! " + date);
-    
-    
+    alert("Приветствуем на LifeSpot! " + session.get("start Date"));
+  
 }
 else{
-   // Выполним проверку. Если введенное число < 18, либо если введено не число -
-   // пользователь будет перенаправлен
-   alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-   window.location.href = "http://www.google.com"
+    // Выполним проверку. Если введенное число < 18, либо если введено не число -
+    // пользователь будет перенаправлен
+    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены"); 
+    window.location.href = "http://www.google.com";
 }
 // Выводим в консоль содержимое объекта Map
-for (let result of session) {
-    console.log(result);
+for (const [key, value] of session) {
+    console.log(`${key}: ${value}`);
 }
