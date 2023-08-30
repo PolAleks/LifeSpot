6e2dd70c888fbb9fcb4d3fed632f2a6332aad433
Александр Полушкин <aa.polushkin@yandex.ru>
2023-08-30 18:30:14 +0300

    unit 28.5.4 complite

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,8 @@
+﻿// Запрос имени пользователя
+let name = prompt('Как вас зовут? ');
+
+// Приветствие посетителя по имени
+/*alert(`Приветствую ${name}! В вашем имени - ${name.length} символ(ов).`)*/
+
+let elements = document.getElementsByTagName('*');
+alert(`Данная html страница содержит ${elements.length} элементов`);
