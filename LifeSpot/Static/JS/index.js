1573dd329076b3c7bd022208f0c9059836b5c553
Александр Полушкин <aa.polushkin@yandex.ru>
2023-09-05 13:54:23 +0300

    Task 29.4.4 complite

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,39 +1,45 @@
-﻿//const handleSession = function() {
-//    // Создаем ассоциативный массив Map для сохранения сессии
-//    let session = new Map();
+﻿function handleSession() {
+    // Создаем ассоциативный массив Map для сохранения сессии
+    let session = new Map();
 
-//    // Сохраняем информацию о браузере клиента
-//    session.set("userAgent", window.navigator.userAgent);
+    // Сохраняем информацию о браузере клиента
+    session.set("userAgent", window.navigator.userAgent);
 
-//    // Запросим возраст пользователя и сохраним в переменную
-//    let age = prompt("Пожалуйста, введите ваш возраст");
+    // Запросим возраст пользователя и сохраним в переменную
+    let age = prompt("Пожалуйста, введите ваш возраст");
 
-//    if (age >= 18) {
-//        // Сохраняем возраст пользователя
-//        session.set("age", age);
+    if (age >= 18) {
+        // Сохраняем возраст пользователя
+        session.set("age", age);
 
-//        // Сохраняем время начала сессии
-//        session.set("startDate", new Date().toLocaleString());
+        // Сохраняем время начала сессии
+        session.set("startDate", new Date().toLocaleString());
 
-//        // Те, кто старше 18, увидят приветствие и будут направлены на сайт
-//        alert("Приветствуем на LifeSpot! " + session.get("startDate"));
+        // Те, кто старше 18, увидят приветствие и будут направлены на сайт
+        alert("Приветствуем на LifeSpot! " + session.get("startDate"));
 
-//    }
-//    else {
-//        // Выполним проверку. Если введенное число < 18, либо если введено не число -
-//        // пользователь будет перенаправлен
-//        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-//        window.location.href = "http://www.google.com";
-//    }
-//    // Выводим в консоль содержимое объекта Map
-//    for (const [key, value] of session) {
-//        console.log(`${key}: ${value}`);
-//    }
-//}
-const filterContent = function() {
-    // Считываем ввод пользователя
-    let searchData = document.getElementsByTagName('input')[0].value.toLowerCase();
+    }
+    else {
+        // Выполним проверку. Если введенное число < 18, либо если введено не число -
+        // пользователь будет перенаправлен
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com";
+    }
 
+    return session;
+}
+
+const sessionLog = function (session) {
+    for (const [key, value] of session) {
+        console.log(`${key}: ${value}`);
+    }
+}
+
+const inputParseFunction = function() {
+    return document.getElementsByTagName('input')[0].value.toLowerCase();
+}
+
+function filterContent(userInput) {
     // Получим все контейнеры с видео
     let elements = document.getElementsByClassName('video-container');
 
@@ -44,7 +50,7 @@ const filterContent = function() {
         let videoDescription = elements[i].getElementsByTagName('h3')[0].innerText.toLowerCase();
 
         // Сравниваем описание видео с поисковой строкой
-        if (!videoDescription.includes(searchData)) {
+        if (!videoDescription.includes(userInput())) {
             // Скрываем видео
             elements[i].style.display = 'none'
         } else {
