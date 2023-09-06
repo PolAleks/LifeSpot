9e4f7e3358789e0d16a1f92e3b725cdb0ebc71fa
Александр Полушкин <aa.polushkin@yandex.ru>
2023-09-06 21:14:03 +0300

    Final task module complite

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,27 @@
+﻿
+let feedback = new Map();
+function getFeedback() {
+    let name = prompt("Представьтесь пожалуйста");
+    if (isFillData(name)) return;
+    feedback.set("name", name);
+
+    let review = prompt("Оставьте свой отзыв");
+    if (isFillData(review)) return;
+    feedback.set("review", review);
+
+    feedback.set("date", new Date().toLocaleString());
+
+    postFeedback();
+}
+
+const postFeedback = () => {
+    let elem = document.getElementsByClassName("reviews")[0];
+    elem.innerHTML += `<div class="review">\n` +
+                        `<p><b>${feedback.get("name")} </b><i>${feedback.get("date")}</i></p>\n` +
+                        `<p>${feedback.get("review")}</p>\n` +
+                      `</div > `;
+}
+
+function isFillData(value) {
+    return value === "";
+}
